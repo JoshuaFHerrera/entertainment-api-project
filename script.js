8d239e1f1ec8eb0036de6522a3d8ba7e61224166
JoshuaFHerrera <josh.f.herrera@gmail.com>
2024-08-10 01:47:37 -0400

    backup files

--- script.js
+++ script.js
@@ -1,38 +1,82 @@
 // "3cb9ca61"
-// initialize movie categories
-let trendingMovieData = [];
-
 // POPUPS
-let firstPopup = true;
-function initializePopup(poster, index) {
+function initializePopup(poster, movie) {
   // nodes
   const popupPoster = document.querySelector(".popup-poster");
-  const rating = document.querySelector(".rating");
-  const ageRating = document.querySelector(".age-rating");
-  const duration = document.querySelector(".duration");
+  const popupPosterImg = document.querySelector(".popup-poster-img");
   // initializing movie attributes
-  let title = poster.title;
-  console.log(poster.title);
+  let movieTitle = movie.Title;
+  let movieYear = movie.Year;
+  let movieDirectors = movie.Director.split(", ");
+  let movieWriters = movie.Writer.split(", ");
+  let movieActors = movie.Actors.split(", ");
+  let movieGenres = movie.Genre.split(", ");
+  let moviePlot = movie.Plot;
+  let movieRated = movie.Rated;
+  let movieRating = movie.imdbRating;
+  let movieRuntime = movie.Runtime;
+
   // opens and closes popups on click
   poster.addEventListener("click", () => {
+    document.body.style.height = "100%";
+    document.body.style.overflow = "hidden";
     document.querySelector(".popup").classList.add("active");
-    if (firstPopup) {
-      // add content if no content exists
-      poster.id = "popup-poster";
-      popupPoster.appendChild(poster);
-      firstPopup = false;
-    } else {
-      // update existing content
-      let currentPoster = document.getElementById("popup-poster");
-      let ratingImg = document.querySelector(".rating-p");
-      let ageImg = document.querySelector(".age-p");
-      let duraitonImg = document.querySelector(".duration-p");
-      currentPoster.src = poster.src;
+    popupPosterImg.src = movie.Poster;
+    let ratingP = document.querySelector(".rating-p");
+    let ageP = document.querySelector(".age-p");
+    let durationP = document.querySelector(".duration-p");
+    let title = document.querySelector(".title");
+    let year = document.querySelector(".year");
+    let directors = document.querySelector(".directors");
+    let genres = document.querySelector(".genres");
+    let writers = document.querySelector(".writers");
+    let actors = document.querySelector(".actors");
+    let plot = document.querySelector(".plot");
+    title.innerHTML = movieTitle;
+    ratingP.innerHTML = movieRating;
+    ageP.innerHTML = movieRated;
+    durationP.innerHTML = movieRuntime;
+    year.innerHTML = movieYear;
+    plot.innerHTML = moviePlot;
+    while (directors.firstChild) {
+      directors.firstChild.remove();
+    }
+    for (let i = 0; i < movieDirectors.length; i++) {
+      let director = document.createElement("li");
+      director.textContent = movieDirectors[i];
+      director.classList.add("test");
+      directors.appendChild(director);
+    }
+    while (writers.firstChild) {
+      writers.firstChild.remove();
+    }
+    for (let i = 0; i < movieWriters.length; i++) {
+      let writer = document.createElement("li");
+      writer.textContent = movieWriters[i];
+      writers.appendChild(writer);
+    }
+    while (actors.firstChild) {
+      actors.firstChild.remove();
+    }
+    for (let i = 0; i < movieActors.length; i++) {
+      let actor = document.createElement("li");
+      actor.textContent = movieActors[i];
+      actors.appendChild(actor);
+    }
+    while (genres.firstChild) {
+      genres.firstChild.remove();
+    }
+    for (let i = 0; i < movieGenres.length; i++) {
+      let genre = document.createElement("li");
+      genre.textContent = movieGenres[i];
+      genres.appendChild(genre);
     }
   });
 }
 // close popups
 function closePopup() {
+  document.body.style.height = 0;
+  document.body.style.overflow = "visible";
   document.querySelector(".popup").classList.remove("active");
 }
 
@@ -52,19 +96,45 @@ fetch("https://api.trakt.tv/movies/trending", {
   .then((response) => response.json())
   // loads each movie into trending and creates its own popup
   .then((data) => {
-    console.log(data);
     for (let i = 0; i < data.length; i++) {
-      let poster = document.createElement("img");
       let link = `http://www.omdbapi.com/?i=${data[i].movie.ids.imdb}&apikey=3cb9ca61`;
       fetch(link)
         .then((response) => response.json())
         .then((data) => {
-          trendingMovieData[i] = data;
           if (data.Poster != "N/A") {
-            let posterSrc = data.Poster;
-            poster.src = posterSrc;
-            initializePopup(poster, i);
-            slidesWrapper.appendChild(poster);
+            let poster = document.getElementById(`trending-${i}`);
+            poster.src = data.Poster;
+            initializePopup(poster, data, "trending");
+          }
+        });
+    }
+  });
+
+// POPULAR MOVIES
+const popularContainer = document.getElementById("popular-container");
+// fetches trending movies
+fetch("https://api.trakt.tv/movies/popular", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "trakt-api-version": 2,
+    "trakt-api-key":
+      "86d2f5aad5605e99d7c434c59c62e226096a9256730e15c6516f3657988baaa0",
+  },
+})
+  .then((response) => response.json())
+  // loads each movie into trending and creates its own popup
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      let link = `http://www.omdbapi.com/?i=${data[i].ids.imdb}&apikey=3cb9ca61`;
+      fetch(link)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.Poster != "N/A") {
+            let poster = document.getElementById(`popular-${i}`);
+            poster.src = data.Poster;
+            initializePopup(poster, data);
           }
         });
     }
